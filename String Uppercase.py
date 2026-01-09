c38939ec2dbc33cd4a9e9e611da3f59852621346
Nadiaiscute2016 <ojinadiaoa@gmail.com>
2026-01-09 18:38:06 +0200

    Yay

---
+++ String Uppercase.py
@@ -0,0 +1,10 @@
+class IOString():
+   def init(self):
+    self.str = ""
+def get_String(self):
+    self.str = input("Enter String: ")
+def print_String(self):
+    print("Result is :", self.str1.upper())
+    str = IOString()
+str.get_String()
+str.print_String()
