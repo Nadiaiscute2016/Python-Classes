0a8a00f24649987c15a0850e225faea29adce30b
Nadiaiscute2016 <ojinadiaoa@gmail.com>
2026-01-23 19:00:11 +0200

    Yay

--- Name The Fruit.py
+++ Name The Fruit.py
@@ -2,50 +2,32 @@ import random
 
 class FruitQuiz:
 
-# Create a constructor
+	# Create a constructor
+	def __init__(self):
+		
+		# Create a dictionary of fruits as keys and color as value
+		self.fruits={'apple':'red',
+					'orange':'orange',
+					'watermelon':'green',
+					'banana':'yellow'}
 
- def  __init__(self):
-
-
-# Create a dictionary of fruits as keys and color as value
-
-  self.fruits={'apple':'red',
-
-'orange':'orange',
-
-'watermelon':'green',
-
-'banana':'yellow'}
-
-# Function for the quiz, here a fruit will be chosen randomly
-
-def quiz(self):
-
-  while (True):
-
-
-   fruit, color = random.choice(list(self.fruits.items()))
-
-
-print("What is the color of {}".format(fruit))
-
-user_answer = input()
-
-
-if(user_answer.lower() == color):
-
-  print("Correct answer")
-
-else:
-
-  print("Wrong answer")
-
-
-option = int(input("enter 0 , if you want to play again otherwise enter 1: "))
-
-if (option):
-
- break
+	# Function for the quiz, here a fruit will be chosen randomly	
+	def quiz(self):
+		while (True):
+			
+			fruit, color = random.choice(list(self.fruits.items()))
+			
+			print("What is the color of {}".format(fruit))
+			user_answer = input()
+			
+			if(user_answer.lower() == color):
+				print("Correct answer")
+			else:
+				print("Wrong answer")
+				
+			option = int(input("enter 0 , if you want to play again otherwise enter 1: "))
+			if (option):
+				break
 
 print("welcome to fruit quiz ")
 
