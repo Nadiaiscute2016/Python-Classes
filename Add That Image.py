a2fa59f8c6a86dff06abd431d1bfb078b1b3b470
Nadiaiscute2016 <ojinadiaoa@gmail.com>
2026-01-24 19:35:32 +0200

    Yay

---
+++ Add That Image.py
@@ -0,0 +1,22 @@
+import pygame 
+pygame.init()
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
+display_surface = pygame.display.set_mode(SCREEN_WIDTH, SCREEN_HEIGHT)
+pygame.display.set_caption('Adding image and bg')
+background_image = pygame.transform.scale(pygame.image.load('Butterfly.jpg').convert(),(SCREEN_WIDTH, SCREEN_HEIGHT))
+character_image = pygame.transform.scale(pygame.image.load('Princess.jpg').convert_alpha(),(200, 200))
+character_rect = character_image.get_rect(center = (SCREEN_WIDTH//2, SCREEN_HEIGHT//2 - 30))
+text = pygame.font.Font(None, 36).render('Hello, World',pygame.color('black'))
+text_rect= text.get(center = (SCREEN_WIDTH//2, SCREEN_HEIGHT//2 + 110)) 
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    display_surface.blit(background_image, (0,0))
+    display_surface.blit(character_image, character_rect)
+    display_surface.blit(text_image, text_rect)
+    pygame.display.flip()
+    clock.tick(30)
+pygame.quit
+if__name__ == '__main__'
+game__loop()
